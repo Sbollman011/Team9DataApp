f12b5fc2472a15f7c48abdee67c3a64979226820
Sbollman011 <58572531+Sbollman011@users.noreply.github.com>
2020-03-16 15:05:19 -0700

    fix

--- src/main/java/com/example/QueryCustomerHistoryService.java
+++ src/main/java/com/example/QueryCustomerHistoryService.java
@@ -17,7 +17,7 @@ public class QueryCustomerHistoryService {
     public List<QueryCustomerHistory> findAll(String customerID) {
         return jdbcTemplate.query("SELECT FirstName, LastName, TransactionID, Name, price " +
         "FROM Customer, Transaction, product, product_sold " +
-        "WHERE TRANS_CustomerID = CustomerID AND SOLD_TransactionID = TransactionID AND SOLD_ProductID = ProductID AND CustomerID = '"+customerID+"'",
+        "WHERE TRANS_CustomerID = CustomerID AND SOLD_TransactionID = TransactionID AND SOLD_ProductID = ProductID AND CustomerID = "+customerID,
         
         (rs, rowNum) -> new QueryCustomerHistory(rs.getString("FirstName"),rs.getString("LastName"),rs.getString("TransactionID"),
         rs.getString("Name"),rs.getString("price")));
